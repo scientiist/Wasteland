d926d48d9a6d2d971ae1e677e0602d6dd15ffd2b
scientiist <j0sh.oleary11@gmail.com>
2021-11-26 18:36:58 -0600

    Removed useless bin and obj folders from repo

--- Wasteland.Client.Desktop.Linux/bin/Debug/netcoreapp5.0/Wasteland.Assets/tileset.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="apocalypse" tilewidth="16" tileheight="16" tilecount="1000" columns="40">
- <editorsettings>
-  <export target="tileset1.json" format="tsx"/>
- </editorsettings>
- <image source="tilesheet.png" trans="000000" width="640" height="400"/>
-</tileset>
