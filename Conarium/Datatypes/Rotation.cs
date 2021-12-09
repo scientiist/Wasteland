1b15f0839f7888fea761f408e9c83e70c5a4df97
scientiist <j0sh.oleary11@gmail.com>
2021-12-09 03:06:11 -0600

    Refactor Rotation.cs & Impl. Arithmetic Operators

--- Conarium/Datatypes/Rotation.cs
+++ Conarium/Datatypes/Rotation.cs
@@ -6,66 +6,151 @@ using System.Collections.Generic;
 namespace Conarium.Datatypes
 {
 
+
     public struct Rotation : IEquatable<Rotation>
     {
-        public static Rotation Zero = FromDeg(0);
-        public static Rotation Pi = FromRad((float)Math.PI);
-        public static Rotation TwoPi = FromRad((float)Math.PI * 2);
-        public static Rotation RightAngle = FromDeg(90);
-
-
+		#region static reference members
+        public static Rotation Zero = new Rotation(0);
+        public static Rotation Pi = new Rotation((float)Math.PI);
+        public static Rotation TwoPi = new Rotation((float)Math.PI * 2);
+        public static Rotation RightAngle = new Rotation(90);
         public const float PI = MathHelper.Pi;
-        private float _radians;
 
-        public float Radians
-        {
-            get { return _radians; }
-            set { _radians = value; }
-        }
-        public float Degrees
-        {
-            get { return _radians * (180.0f / PI); }
-            set { _radians = value * (PI / 180.0f); }
-        }
+		#endregion
 
 
-        public Vector2 ToUnitVector()
-        {
-            return new Vector2((float)Math.Cos(Radians), (float)Math.Sin(Radians));
-        }
+        public float Radians { get;set; }
+		public float NormalizedRadians => Radians%TwoPi.Radians;
+		public float Degrees 
+		{
+			get => MathHelper.ToDegrees(Radians);
+			set => Radians = MathHelper.ToRadians(value);
+		}
+		public float NormalizedDegrees => Degrees%TwoPi.Degrees;
 
-        public static Rotation FromUnitVector(Vector2 vec)
-        {
-            vec.Normalize();
-            return Rotation.FromRad((float)Math.Atan2(vec.Y, vec.X));
-        }
+		// Static Math Formulae
+		public static float UnitVectorToRadians(Vector2 unit)
+		{
+			var copyVec = new Vector2(unit.X, unit.Y);
+			copyVec.Normalize();
+            return (float)Math.Atan2(copyVec.Y, copyVec.X);
+		}
+		public static Vector2 RadiansToUnitVector(float rads)
+		{
+			return new Vector2(
+				x: (float)Math.Cos(rads),
+				y: (float)Math.Sin(rads)
+			);
+		}
 
-        public static Rotation FromRad(float radians)
-        {
-            return new Rotation { Radians = radians };
-        }
+        public Vector2 ToUnitVector() => RadiansToUnitVector(this.Radians);
 
-        public static Rotation FromDeg(float degree)
-        {
-            return new Rotation { Degrees = degree };
-        }
 
-        public Rotation RotateDeg(float degree) => FromDeg(this.Degrees + degree);
+		public Rotation(float radians)
+		{
+			Radians = radians;
+		}
 
-        public Rotation RotateRad(float radians) => FromRad(this.Radians + radians);
+		public Rotation(Vector2 unitVector)
+		{
+			Radians = UnitVectorToRadians(unitVector);
+		}
 
-        public bool EqualsRadians(float rad, float toleranceDegrees = 1.0f)
+        //public static Rotation FromDeg(float degree)
+        //{
+            //return new Rotation { Radians = MathHelper.ToRadians(degree) }; //Degrees = degree };
+        //}
+ 
+        //public Rotation RotateDeg(float degree) => FromDeg(this.Degrees + degree);
+
+        //public Rotation RotateRad(float radians) => new Rotation(this.Radians + radians);
+
+		public void Normalize()
+		{
+			Radians = Radians%TwoPi.Radians;
+		}
+
+		#region Operators
+		// having to write these operators both ways is a hassle
+
+		// arithmetic operators
+		public static Rotation operator +(Rotation a, Rotation b)
+			=> new Rotation(a.Radians + b.Radians);
+		public static Rotation operator -(Rotation a, Rotation b)
+			=> new Rotation(a.Radians - b.Radians);
+		public static Rotation operator +(Rotation a, float b)
+			=> new Rotation(a.Radians + b);
+		public static Rotation operator -(Rotation a, float b)
+			=> new Rotation(a.Radians - b);
+		public static Rotation operator +(float a, Rotation b)
+			=> new Rotation(a + b.Radians);
+
+		public static Rotation operator -(float a, Rotation b)
+			=> new Rotation(a + b.Radians);
+
+		// comparison operators
+		public static bool operator >(Rotation a, Rotation b) => a.Radians > b.Radians;
+		public static bool operator <(Rotation a, Rotation b) => a.Radians < b.Radians;
+		public static bool operator >(Rotation a, float b) => a.Radians > b;
+		public static bool operator <(Rotation a, float b) => a.Radians < b;
+		public static bool operator >(float a, Rotation b) => a > b.Radians;
+		public static bool operator <(float a, Rotation b) => a < b.Radians;
+
+		public static bool operator >=(Rotation a, Rotation b)
+			=> a.Radians >= b.Radians;
+		public static bool operator <=(Rotation a, Rotation b)
+			=> a.Radians <= b.Radians;
+		public static bool operator >=(Rotation a, float b)
+			=> a.Radians >= b;
+		public static bool operator <=(Rotation a, float b)
+			=> a.Radians <= b;
+		public static bool operator >=(float a, Rotation b)
+			=> a >= b.Radians;
+		public static bool operator <=(float a, Rotation b)
+			=> a <= b.Radians;
+		// modulus operators
+		
+		public static Rotation operator %(Rotation a, Rotation b) 
+			=> new Rotation(a.Radians % b.Radians);
+		public static Rotation operator %(Rotation a, float b) 
+			=> new Rotation(a.Radians % b);
+		public static Rotation operator %(float a, Rotation b) 
+			=> new Rotation(a % b.Radians);
+
+		public static bool operator ==(Rotation a, Rotation b) => a.EqualsExact(b);
+		public static bool operator !=(Rotation a, Rotation b) => !a.EqualsExact(b);
+
+
+        public bool CloseEnough(float rad, float toleranceDegrees = 1.0f)
         {
             return Math.Abs(Radians - rad) < (toleranceDegrees * (PI / 180.0f));
         }
-        public bool EqualsDegrees(float deg, float toleranceDegrees = 1.0f)
-        {
-            return Math.Abs(Degrees - deg) < toleranceDegrees;
-        }
 
-        public bool Equals(Rotation other)
-        {
-            return EqualsRadians(other.Radians);
-        }
-    }
+		public bool CloseEnough(Rotation rot, float toleranceDegrees = 1.0f)
+			=> CloseEnough(rot.Radians, toleranceDegrees);
+
+		public bool EqualsExact(float rad) => this.Radians == rad;
+		public bool EqualsExact(Rotation rot) => EqualsExact(rot.Radians);
+		// defined for IEquatable
+		public bool Equals(Rotation compare) => EqualsExact(compare);
+
+
+		public override bool Equals(object obj)
+		{
+			return base.Equals(obj);
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+
+
+		#endregion
+	}
 }
